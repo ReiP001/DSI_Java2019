75ad8c5c83e33b6fb3c6a8a6253acdcfdeafbe68
ReiP001 <nekopier@gmail.com>
2019-05-14 11:57:52 -0500

    mensaje

---
+++ mensaje.java
@@ -0,0 +1,9 @@
+public class Mensaje{
+    
+    public static void main(String[] args){
+        System.out.println("¡BIENVENIDOS!!!");
+	System.out.println("Ahora estamos en el Modulo IV. ");
+	System.out.println("Prometo esforzarme día a día, mucho más que el modulo anterior ");
+    }
+    
+}
